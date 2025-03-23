b28d18ba84ef64e07004e5828a630ec2d536c4dc
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2025-03-23 10:28:34 +0400

    Embedding utils

--- test2text/embeddings/embed.py
+++ test2text/embeddings/embed.py
@@ -9,3 +9,12 @@ logger.debug('Model loaded')
 
 def embed_requirement(requirement: str) -> np.ndarray:
     return model.encode(['search_document: ' + requirement])[0]
+
+def embed_requirements_batch(requirements: list[str]) -> np.ndarray:
+    return model.encode(['search_document: ' + requirement for requirement in requirements])
+
+def embed_annotation(annotation: str) -> np.ndarray:
+    return model.encode(['search_query: ' + annotation])[0]
+
+def embed_annotations_batch(annotations: list[str]) -> np.ndarray:
+    return model.encode(['search_query: ' + annotation for annotation in annotations])
