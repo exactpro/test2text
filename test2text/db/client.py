9d922ad4a5c1b370739fc4c28d4ca96af21121e4
Nikolai Dorofeev - d0rich <contact@d0rich.me>
2025-07-14 08:37:30 +0400

    Check SQLite version

--- test2text/db/client.py
+++ test2text/db/client.py
@@ -2,6 +2,7 @@ import sqlite3
 import sqlite_vec
 import logging
 
+from test2text.utils.semver import Semver
 from .tables import RequirementsTable, AnnotationsTable, AnnotationsToRequirementsTable, TestCasesTable, TestCasesToAnnotationsTable
 from ..utils.path import PathParam
 
@@ -10,7 +11,18 @@ logger = logging.getLogger(__name__)
 class DbClient:
     conn: sqlite3.Connection
 
+    @staticmethod
+    def _check_sqlite_version():
+        # Version when RETURNED is available
+        REQUIRED_SQLITE_VERSION = Semver('3.35.0')
+        sqlite_version = Semver(sqlite3.sqlite_version)
+        if sqlite_version < REQUIRED_SQLITE_VERSION:
+            raise RuntimeError(f'SQLite version {sqlite_version} is too old. '
+                               f'Required version is {REQUIRED_SQLITE_VERSION}. '
+                               'Please upgrade SQLite in your system to use this feature.')
+
     def __init__(self, file_path: PathParam, embedding_dim: int = 768):
+        self._check_sqlite_version()
         logger.info('Connecting to database at %s', file_path)
         self.conn = sqlite3.connect(file_path)
         self.embedding_dim = embedding_dim
