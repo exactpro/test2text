9a06438a694dc812b3333a39629440efdbd46316
anna.yamkovaya <anna.yamkovaya@exactprosystems.com>
2025-07-18 11:31:05 +0000

    add upload annotations and requirements pages

--- pages/upload/requirements.py
+++ pages/upload/requirements.py
@@ -1,51 +1,26 @@
-from typing import Optional
+import streamlit as st
+import csv
 
-from .abstract_table import AbstractTable
-from sqlite3 import Connection
-from sqlite_vec import serialize_float32
-from string import Template
+st.title("Upload a file with requirements.")
 
+uploaded_file = st.file_uploader("Choose a file with requirements", type="csv")
 
-class RequirementsTable(AbstractTable):
-    def __init__(self, connection: Connection, embedding_size: int):
-        super().__init__(connection)
-        self.embedding_size = embedding_size
+if uploaded_file is not None:
+    try:
+        # Read the file as a string
+        with open(uploaded_file, "r") as trace_file:
+            reader = csv.reader(trace_file)
+            rows = list(reader)
+            # run indexing
 
-    def init_table(self):
-        self.connection.execute(
-            Template("""
-            CREATE TABLE IF NOT EXISTS Requirements (
-                id INTEGER PRIMARY KEY AUTOINCREMENT ,
-                external_id TEXT UNIQUE,
-                summary TEXT UNIQUE NOT NULL,
-                embedding float[$embedding_size],
-
-                CHECK (
-                    typeof(embedding) == 'null' or
-                    (typeof(embedding) == 'blob' 
-                        and vec_length(embedding) == $embedding_size)
-                )
-            )
-            """).substitute(embedding_size=self.embedding_size)
-        )
-
-    def insert(
-        self, summary: str, embedding: list[float] = None, external_id: str = None
-    ) -> Optional[int]:
-        cursor = self.connection.execute(
-            """
-            INSERT OR IGNORE INTO Requirements (summary, embedding, external_id)
-            VALUES (?, ?, ?)
-            RETURNING id
-            """,
-            (
-                summary,
-                serialize_float32(embedding) if embedding is not None else None,
-                external_id,
-            ),
-        )
-        result = cursor.fetchone()
-        if result:
-            return result[0]
+        if rows:
+            st.success("CSV file uploaded and parsed successfully!")
+            st.subheader("Extracted Rows:")
+            # Display as a table
+            st.table(rows)
         else:
-            return None
+            st.warning("The uploaded CSV file is empty.")
+    except Exception as e:
+        st.error(f"Failed to parse CSV file: {e}")
+else:
+    st.info("Please upload a file with requirements to extract rows.")
