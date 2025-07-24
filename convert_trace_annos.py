85d64939d4762c9e6fbdb4901038332c69f957c8
anna.yamkovaya <anna.yamkovaya@exactprosystems.com>
2025-07-24 16:10:41 +0000

    improved reading of trace files

--- convert_trace_annos.py
+++ convert_trace_annos.py
@@ -12,23 +12,37 @@ def trace_test_cases_to_annos(db_path: Path, trace_file_path: Path):
     test_cases = set()
     logger.info("Reading trace file and inserting annotations into table...")
     with open(trace_file_path, mode='r', newline='', encoding='utf-8') as trace_file:
-        reader = csv.DictReader(trace_file)
+        reader = csv.reader(trace_file)
         current_tc = ""
         concat_summary = ""
+        test_script = ""
+        global_columns = next(reader)
         for row in reader:
-            if row.get("TestCase", "") != "":
-                if current_tc != row["TestCase"]:
-                    if current_tc:
-                        case_id = db.test_cases.insert(test_script=row["TestScript"], test_case=row["TestCase"])
-                        annotation_id = db.annotations.insert(summary=concat_summary)
-                        db.cases_to_annos.insert(case_id=case_id, annotation_id=annotation_id)
-                        concat_summary = ""
-                    current_tc = row["TestCase"]
+            if row[0] == "TestCaseStart":
+                current_tc = row[1]
+                test_script = ""
+                concat_summary = ""
+                if current_tc != "":
                     test_cases.add(current_tc)
-                concat_summary += row["Summary"]
+                next(reader)
+            elif row[0] == "Summary":
+                continue
+            elif row[0] == "TestCaseEnd":
+                if current_tc != "" and concat_summary != "":
+                    case_id = db.test_cases.insert(test_script=test_script, test_case=current_tc)
+                    annotation_id = db.annotations.insert(summary=concat_summary)
+                    db.cases_to_annos.insert(case_id=case_id, annotation_id=annotation_id)
+            else:
+                if row[global_columns.index("TestCase")] != "":
+                    if current_tc != row[global_columns.index("TestCase")]:
+                        current_tc = row[global_columns.index("TestCase")]
+                        test_cases.add(current_tc)
+                    if test_script == "" and row[global_columns.index("TestScript")] != "":
+                        test_script = row[global_columns.index("TestScript")]
+                    concat_summary += row[0]
 
     db.conn.commit()
-    logger.info(f"Inserted {len(test_cases)} testcase-annotations pairs to database.")
+    logger.info(f"Inserted {len(test_cases)} testcase-annotations pairs to database.{[tc for tc in test_cases]}")
 
 
 if __name__ == '__main__':
