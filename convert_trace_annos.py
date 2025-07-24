0aa3389522466ac34b76e745add1ef81dfa2d3e5
anna.yamkovaya <anna.yamkovaya@exactprosystems.com>
2025-07-24 17:08:04 +0000

    couple of improvements

--- convert_trace_annos.py
+++ convert_trace_annos.py
@@ -6,6 +6,13 @@ from test2text.db import DbClient
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+EMPTY = ""
+
+
+def is_empty(value):
+    return True if value == EMPTY else False
+
+
 def trace_test_cases_to_annos(db_path: Path, trace_file_path: Path):
     db = DbClient(db_path)
 
@@ -13,36 +20,36 @@ def trace_test_cases_to_annos(db_path: Path, trace_file_path: Path):
     logger.info("Reading trace file and inserting annotations into table...")
     with open(trace_file_path, mode='r', newline='', encoding='utf-8') as trace_file:
         reader = csv.reader(trace_file)
-        current_tc = ""
-        concat_summary = ""
-        test_script = ""
+        current_tc = EMPTY
+        concat_summary = EMPTY
+        test_script = EMPTY
         global_columns = next(reader)
         for row in reader:
             if row[0] == "TestCaseStart":
                 current_tc = row[1]
-                test_script = ""
-                concat_summary = ""
-                if current_tc != "":
+                test_script = EMPTY
+                concat_summary = EMPTY
+                if not is_empty(current_tc):
                     test_cases.add(current_tc)
                 next(reader)
             elif row[0] == "Summary":
                 continue
             elif row[0] == "TestCaseEnd":
-                if current_tc != "" and concat_summary != "":
+                if not is_empty(current_tc) and not is_empty(concat_summary):
                     case_id = db.test_cases.insert(test_script=test_script, test_case=current_tc)
                     annotation_id = db.annotations.insert(summary=concat_summary)
                     db.cases_to_annos.insert(case_id=case_id, annotation_id=annotation_id)
             else:
-                if row[global_columns.index("TestCase")] != "":
+                if not is_empty(row[global_columns.index("TestCase")]):
                     if current_tc != row[global_columns.index("TestCase")]:
                         current_tc = row[global_columns.index("TestCase")]
                         test_cases.add(current_tc)
-                    if test_script == "" and row[global_columns.index("TestScript")] != "":
+                    if is_empty(test_script) and not is_empty(row[global_columns.index("TestScript")]):
                         test_script = row[global_columns.index("TestScript")]
                     concat_summary += row[0]
 
     db.conn.commit()
-    logger.info(f"Inserted {len(test_cases)} testcase-annotations pairs to database.{[tc for tc in test_cases]}")
+    logger.info(f"Inserted {len(test_cases)} testcase-annotations pairs to database.")
 
 
 if __name__ == '__main__':
