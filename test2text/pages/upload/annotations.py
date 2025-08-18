dbdd555fcf13591ee7f37a14ae7af7accb7e17a9
Nikolai Dorofeev - d0rich <contact@d0rich.me>
2025-08-18 16:19:43 +0400

    Improved information on upload annotations form

--- test2text/pages/upload/annotations.py
+++ test2text/pages/upload/annotations.py
@@ -1,43 +1,54 @@
 import streamlit as st
 
+LOAD_TRACE_FILE = "Load from trace file"
+LOAD_AND_CONCAT_TRACE_FILE = "Load and concatenate trace files"
+
 
 def show_annotations():
-    with st.form("Upload *.trace.csv file"):
-        st.header("Upload *.trace.csv file")
+    st.header("Upload *.trace.csv file")
 
-        st.subheader("1. Choose an action to execute")
-
-        chosen_option = st.selectbox(
-            "Choose an action:",
-            ("Write test cases and their annotations to database", "Index annotations"),
+    st.subheader("1. Choose an action to execute")
+    chosen_option = st.selectbox(
+        "Choose an action:", (LOAD_TRACE_FILE, LOAD_AND_CONCAT_TRACE_FILE)
+    )
+    if chosen_option == LOAD_TRACE_FILE:
+        st.info(
+            "All annotations will be loaded as is. Each line will represent an annotation occurrence."
         )
+    elif chosen_option == LOAD_AND_CONCAT_TRACE_FILE:
+        st.info(
+            "Annotations will be grouped by test cases and concatenated, For each test case only 1 very detailed annotation will be created."
+        )
+
+    with st.form("upload_annotations_form"):
         st.subheader("2. Choose *.trace.csv files")
         uploaded_files = st.file_uploader(
-            "Choose files", type="csv", accept_multiple_files=True
+            "Choose files", type="trace.csv", accept_multiple_files=True
         )
 
         if not uploaded_files:
             st.info("Please upload a *.trace.csv file to extract annotations.")
-            submitted = st.form_submit_button("Start")
-            return False
 
-        st.success("CSV file uploaded successfully!")
         submitted = st.form_submit_button("Start")
 
     if submitted:
         st.subheader("Results:")
-        if chosen_option == "Index annotations":
-            from test2text.services.loaders.index_annotations import (
-                index_annotations_from_files,
-            )
+        with st.spinner("Loading trace files...", show_time=True):
+            if chosen_option == LOAD_TRACE_FILE:
+                from test2text.services.loaders.index_annotations import (
+                    index_annotations_from_files,
+                )
 
-            index_annotations_from_files(uploaded_files)
-        else:
-            from test2text.services.loaders.convert_trace_annos import (
-                trace_test_cases_to_annos,
-            )
+                index_annotations_from_files(uploaded_files)
 
-            trace_test_cases_to_annos(uploaded_files)
+            elif chosen_option == LOAD_AND_CONCAT_TRACE_FILE:
+                from test2text.services.loaders.convert_trace_annos import (
+                    trace_test_cases_to_annos,
+                )
+
+                trace_test_cases_to_annos(uploaded_files)
+            else:
+                st.error("Unknown action selected. Please try again.")
 
 
 if __name__ == "__main__":
